f1ce982dfda5dafd9520c36cb2d694346ad86800
Chiara Campalani <campalanichiara@gmail.com>
2021-06-22 15:12:46 +0900

    created Record model & validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,7 @@
 class Record < ApplicationRecord
   belongs_to :user
 
-  
+  validates :total_cal, presence: true, numericality: true
+  validates :date, presence: true
+  vaidates :meal_type, presence: true, inclusion: { in: %w(breakfast, lunch, snack, dinner) }
 end
