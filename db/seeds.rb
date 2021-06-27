cd93e663402f8eb1b98df26f7b886d058fad68bc
Chiara Campalani <campalanichiara@gmail.com>
2021-06-27 14:12:21 +0900

    created seeds for Foods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Cleaning out old junk~'
+Food.destroy_all
+puts 'Database shiny & new! Ready to seed:'
+
+puts 'Creating 10 random foods...'
+10.times do
+  food = Food.new(
+    food_name: Faker::Food.ingredient,
+    quantity:  100,
+    calories: rand(0..1000),
+    serving_unit: 'grams'
+  )
+  puts `Created #{food.name}..`
+  food.save!
+  puts '..and added to the pantry!'
+end
+puts "Created all fake foods! Fridge's full!"
